0932d6cf03f81aececfb7afb540dd8df2b80bf49
Lucauz09 <90978354+User-790@users.noreply.github.com>
2025-04-28 23:54:54 -0400

    Update save-sam-n-sys.bat

--- save-sam-n-sys.bat
+++ save-sam-n-sys.bat
@@ -1,4 +1,4 @@
 reg save HKLM\SAM C:\Users\Public\SAM.save
 reg save HKLM\SYSTEM C:\Users\Public\SYSTEM.save
 
-REM Saves this cotent in the Public user folder
+REM Saves this cotent in the Public user folder (run as admin)
